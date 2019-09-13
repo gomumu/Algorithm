70e1f9c28cc5f174d5ac6380aa17220b267c2466
gomumu <g.gomumu@gmail.com>
2019-09-13 11:09:52 +0900

    삽입정렬 수정

--- sort/InsertSort/main.cpp
+++ sort/InsertSort/main.cpp
@@ -6,12 +6,14 @@ const int last = 10;
 std::array<int, last> val = { 3,42,11,34,56, 72, 97, 84, 23, 10 };
 
 void insertSort(std::array<int, last>& arr, int last) {
-	for (int i = 1; i < last; ++i) {
-		if (arr.at(i - 1) > arr.at(i)) {
-			for (int j = 0; j < last; ++j) {
+	for (int i = 0; i < last; ++i) {
+		if ( i-1 > 0 && (arr.at(i - 1) > arr.at(i))) {
+			// search
+			for (int j = 0; j < i; ++j) {
 				if (arr.at(j) > arr.at(i)) {
 					int temp = arr.at(j);
 					arr.at(j) = arr.at(i);
+					arr.at(i) = temp;
 				}
 			}
 		}
